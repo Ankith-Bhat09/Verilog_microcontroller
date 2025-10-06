3e1ff7c06fc25becc3322a2bdfc4dac9eec21306
Ankith-Bhat09 <ankithbhat09@email.com>
2025-10-06 13:25:10 +0530

    wrote Exe part for rr_alu

--- RTL Code/processor.v
+++ RTL Code/processor.v
@@ -14,12 +14,20 @@ parameter shiftR=6'b010001, shiftL=6'b010010;
 parameter branch=6'b100000, Jmp=6'b100001, Beq=6'b100010, Blt=6'b100011,Bgt=6'b100100;
 parameter Nop=6'b000000;
 parameter Halt=6'b111111;
+
+//***********************************************************************\\
+// Operation type
+parameter rr_ALU=4'b0001, ri_ALU=4'b0010, ri_Load=4'b0011, ri_Store=4'b0100;
+parameter ri_shiftR=4'b0101, ri_shiftL=4'b0110;
+parameter branch=4'b0111;
+parameter rr_Nop=4'b1000, rr_Halt=4'b1001;
 //***********************************************************************\\
 //Internal register and wires
 reg [31:0] PC,if_id_ir;
 reg [31:0] mem[0:255];
 reg [31:0] regfile[0:31];
 reg halted;
+reg taken_branch;
 //***********************************************************************\\
 // Instruction Fetch
 always @(posedge clk1)
@@ -51,33 +59,35 @@ begin
     end
     if(if_id_ir[25:21]==5'b00000) id_ex_a<=0;
     else id_ex_a<=#2 regfile[if_id_ir[25:21]];
+
     if(if_id_ir[20:16]==5'b00000) id_ex_b<=0;
     else id_ex_b<=#2 regfile[if_id_ir[20:16]];
+
+    id_ex_imm<= {{16{if_id_ir[15]}},if_id_ir[15:0]};
     id_ex_ir<=if_id_ir;
     id_ex_npc<=if_id_npc;
-    id_ex_imm<= {{16{if_id_ir[15]}},if_id_ir[15:0]};
 
     case (if_id_ir[31:26])
-        ADD: id_ex_op<=rr_ADD;  // Add
-        SUB: id_ex_op<=rr_SUB;  // Subtract
-        AND: id_ex_op<=rr_AND;  // And
-        OR: id_ex_op<=rr_OR;    // Or
-        NOT: id_ex_op<=rr_XOR;  // Not
-        ADDI: id_ex_op<=ri_ADDI;  // Add Immediate
-        SUBI: id_ex_op<=ri_SUBI;  // Subtract Immediate
-        ANDI: id_ex_op<=ri_ANDI;  // And Immediate
-        ORI:  id_ex_op<=ri_ORI;   // Or Immediate
+        ADD: id_ex_op<=rr_ALU;  // Add
+        SUB: id_ex_op<=rr_ALU;  // Subtract
+        AND: id_ex_op<=rr_ALU;  // And
+        OR: id_ex_op<=rr_ALU;    // Or
+        NOT: id_ex_op<=rr_ALU;  // Not
+        ADDI: id_ex_op<=ri_ALU;  // Add Immediate
+        SUBI: id_ex_op<=ri_ALU;  // Subtract Immediate
+        ANDI: id_ex_op<=ri_ALU;  // And Immediate
+        ORI:  id_ex_op<=ri_ALU;   // Or Immediate
         Load: id_ex_op<=ri_Load;  // Load
         Store:id_ex_op<=ri_Store; // Store
         shiftR:id_ex_op<=ri_shiftR; // Shift Right
         shiftL:id_ex_op<=ri_shiftL; // Shift Left
-        branch:id_ex_op<=jp_branch; // Branch
-        Jmp:   id_ex_op<=rp_Jmp;   // Jump
-        Beq:   id_ex_op<=rp_Beq;   // Branch if Equal
-        Blt:   id_ex_op<=rp_Blt;   // Branch if Less Than
-        Bgt:   id_ex_op<=rp_Bgt;   // Branch if Greater Than
-        Nop:   id_ex_op<=Nop;   // No Operation
-        Halt:  id_ex_op<=Halt;  // Halt
+        branch:id_ex_op<=branch; // Branch
+        Jmp:   id_ex_op<=branch;   // Jump
+        Beq:   id_ex_op<=branch;   // Branch if Equal
+        Blt:   id_ex_op<=branch;   // Branch if Less Than
+        Bgt:   id_ex_op<=branch;   // Branch if Greater Than
+        Nop:   id_ex_op<=rr_Nop;   // No Operation
+        Halt:  id_ex_op<=rr_Halt;  // Halt
         default: id_ex_op<=Nop;
     endcase
 end
@@ -86,7 +96,33 @@ end
 // Execute
 always @(posedge clk1)
 begin
+    if(halted==0)
+    begin
+        ex_mem_ir<=id_ex_ir;
+        ex_mem_op<=id_ex_op;
+        taken_branch<=0;
+        case(id_ex_op)
+            rr_ALU: case(id_ex_ir[31:26])
+                        ADD: ex_mem_ALUout <= id_ex_a + id_ex_b; // ADD
+                        SUB: ex_mem_ALUout <= id_ex_a - id_ex_b; // SUB
+                        AND: ex_mem_ALUout <= id_ex_a & id_ex_b; // AND
+                        OR:  ex_mem_ALUout <= id_ex_a | id_ex_b; // OR
+                        XOR: ex_mem_ALUout <= id_ex_a ^ id_ex_b; // XOR
+                        default: ex_mem_ALUout <=0;
+                    endcase
 
+        endcase
+    end
+end
+//***********************************************************************\\
+// Memory
+always @(posedge clk2)
+begin
+
+            ex_mem_ALUout <= id_ex_a + id_ex_imm; // ADDI
+            ex_mem_ALUout <= id_ex_a - id_ex_imm; // SUBI
+        endcase
+    end
 end
 //***********************************************************************\\
 // Memory
