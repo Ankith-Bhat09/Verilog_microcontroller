4b08fd4726201c75e82297aaac28541d3e8b162c
Ankith-Bhat09 <160859053+Ankith-Bhat09@users.noreply.github.com>
2025-10-06 12:46:38 +0530

    Delete Instruction memory directory

--- Instruction memory/instruction_mem.v
+++
@@ -1,7 +0,0 @@
-module instr_mem (input wire [31:0]address, output wire [31:0]instruction);
-reg [31:0] mem [0:127];
-    initial begin
-        $readmemh("instructions.hex", mem);
-    end
-    assign instruction = mem[address];
-endmodule
