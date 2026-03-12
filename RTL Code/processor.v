8dadc24c7cf7ee9f9024da793969287b7ac2a397
Ankith-Bhat09 <160859053+Ankith-Bhat09@users.noreply.github.com>
2026-03-12 18:33:27 +0530

    Refactor processor module and add pipelined MIPS32

--- RTL Code/processor.v
+++ RTL Code/processor.v
@@ -1,270 +1,135 @@
-module processor(clk1, clk2, reset);
-//Input and output ports
-input clk1, clk2;
-input reset;
-//***********************************************************************\\
-// Parameters
-// Register type
-parameter ADD=6'b000001;    // ADD rs1 rs2 rd
-parameter SUB=6'b000010;    // SUB rs1 rs2 rd
-parameter AND=6'b000011;    // AND rs1 rs2 rd
-parameter OR=6'b000100;     // OR rs1 rs2 rd
-parameter XOR=6'b000101;    // XOR rs1 rs2 rd
-// Immediate type
-parameter ADDI=6'b000110;   // ADDI rs1 rd imm
-parameter SUBI=6'b000111;   // SUBI rs1 rd imm
-parameter ANDI=6'b001000;   // ANDI rs1 rd imm
-parameter ORI=6'b001001;    // ORI rs1 rd imm
-parameter Load=6'b001010;   // Load rs1 rd
-parameter Store=6'b001011;  // Store rs1 rd
+module pipe_mips32 (clk1, clk2);
+  input clk1, clk2;
+  reg [31:0] PC, IF_ID_IR, IF_ID_NPC;
 
-// Immediate type with check for opcode
+  reg [31:0] ID_EX_IR, ID_EX_NPC, ID_EX_A, ID_EX_B, ID_EX_Imm;
+  reg [2 :0] ID_EX_type, EX_MEM_type, MEM_WB_type;
 
-parameter shiftR=6'b010001; // shiftR rs1 rd imm
-parameter shiftL=6'b010010; // shiftL rs1 rd imm
-parameter branch=6'b100000; // branch rs1 imm
-parameter Jmp=6'b100001;    // Jmp imm
-parameter Beq=6'b100010;    // Beq rs1 rs2 imm
-parameter Blt=6'b100011;    // Blt rs1 rs2 imm
-parameter Bgt=6'b100100;    // Bgt rs1 rs2 imm
-parameter Nop=6'b000000;    // Nop
-parameter Halt=6'b111111;   // Halt
+  reg [31:0] EX_MEM_IR, EX_MEM_ALUOut, EX_MEM_B;
+  reg EX_MEM_cond;
+  reg [31:0] MEM_WB_IR, MEM_WB_ALUOut, MEM_WB_LMD;
+  reg [31:0] Reg [0:31];
+  reg [31:0] Mem [0:1023];
+// MOVI instruction assumes source register as r0 by default
+  parameter ADD=6'b000000, SUB=6'b000001, AND=6'b000010, OR=6'b000011,
+            SLT=6'b000100, MUL=6'b000101, MOVI=6'b000110, HLT=6'b111111, LW=6'b001000, // Corrected LW opcode
+            SW=6'b001001, ADDI=6'b001010, SUBI=6'b001011,SLTI=6'b001100,SRTI=6'b001111,
+            BNEQZ=6'b001101, BEQZ=6'b001110;
 
-//***********************************************************************\\
-// Operation type
-parameter rr_ALU=4'b0001, ri_ALU=4'b0010, ri_Load=4'b0011, ri_Store=4'b0100;
-parameter ri_shiftR=4'b0101, ri_shiftL=4'b0110;
-parameter r_branch=4'b0111;
-parameter r_Nop=4'b1000, r_Halt=4'b1001;
-//***********************************************************************\\
-//Internal register and wires
-reg [31:0] PC,if_id_ir;
-reg [31:0] mem[0:255];
-reg [31:0] regfile[0:31];
-integer i;
-reg halted;
-reg taken_branch;
-reg [31:0] if_id_npc, id_ex_npc;
-reg [31:0] id_ex_a, id_ex_b, id_ex_imm;
-reg [31:0] id_ex_ir;
-reg [3:0] id_ex_optype;
-reg [31:0] ex_mem_ir;
-reg [3:0] ex_mem_optype;
-reg [31:0] ex_mem_ALUout, ex_mem_b;
-reg ex_mem_cond;
-reg [31:0] mem_wb_ir;
-reg [3:0] mem_wb_optype;
-reg [31:0] mem_wb_ALUout, mem_wb_Loadout;
-always @(reset) begin
-        PC <= 0;
-        if_id_ir <= 0;
-        if_id_npc <= 0;
-        halted <= 0;
-        taken_branch <= 0;
-        // Clear register file and memory on reset to avoid X state
-        for (i = 0; i < 32; i = i + 1) begin
-            regfile[i] <= 0;
-        end
-        for (i = 0; i < 256; i = i + 1) begin
-            mem[i] <= 0;
-        end
-end
-
-//***********************************************************************\\
-// Instruction Fetch
-always @(posedge clk1)
-begin
-    if (halted==0)
+  parameter RR_ALU=3'b000, RM_ALU=3'b001, LOAD=3'b010, STORE=3'b011,
+            BRANCH=3'b100, HALT=3'b101;
+  reg HALTED;
+  reg TAKEN_BRANCH;
+/*********************************************************************/
+  always @ (posedge clk1)// IF Stage
+    if (HALTED == 0)
     begin
-        if(((ex_mem_ir[31:26]==branch)&&(ex_mem_cond == 1))||((ex_mem_ir[31:26]==Jmp)&&(ex_mem_cond == 1))||((ex_mem_ir[31:26]==Beq)&&(ex_mem_cond == 1))||((ex_mem_ir[31:26]==Blt)&&(ex_mem_cond == 1))||((ex_mem_ir[31:26]==Bgt)&&(ex_mem_cond == 1)))
+      if (((EX_MEM_IR[31:26] == BEQZ) && (EX_MEM_cond == 1)) ||((EX_MEM_IR[31:26] == BNEQZ) && (EX_MEM_cond == 0) ) )
         begin
-            if_id_ir <= mem[ex_mem_ALUout];
-            taken_branch <= 1;
-            if_id_npc <= ex_mem_ALUout + 1;
-            PC <= ex_mem_ALUout + 1;
+          IF_ID_IR<= #2 Mem[EX_MEM_ALUOut];
+          TAKEN_BRANCH<= #2 1'b0;
+          IF_ID_NPC<= #2 EX_MEM_ALUOut + 1;
+          PC<= #2 EX_MEM_ALUOut + 1;
         end
-        else
+      else
         begin
-            if_id_ir<=mem[PC];
-            PC<=PC+1;
-            if_id_npc<=PC+1;
+          IF_ID_IR<= #2 Mem[PC] ;
+          IF_ID_NPC<= #2 PC + 1;
+          PC<= #2 PC + 1;
         end
     end
-end
-//***********************************************************************\\
-// Instruction Decode
-always @(posedge clk2)
-begin
-//*********** check for debug here This may be a problem. ***********\\
-//if (halted==0)
-//begin
-//    if(if_id_ir[31:26]==Jmp)
-//    begin
-//        PC<=if_id_ir[25:0];
-//end
-//*********** check for debug here This may be a problem. ***********\\
-    if(if_id_ir[25:21]==5'b00000) id_ex_a<=0;
-    else id_ex_a<=#2 regfile[if_id_ir[25:21]];
+/*********************************************************************/
+  always @(posedge clk2)// ID Stage
+    if (HALTED == 0)
+      begin
+        if (IF_ID_IR[25:21] == 5'b00000) ID_EX_A <= 0;
+        else ID_EX_A<= #2 Reg[IF_ID_IR[25:21] ]; 
 
-    if(if_id_ir[20:16]==5'b00000) id_ex_b<=0;
-    else id_ex_b<=#2 regfile[if_id_ir[20:16]];
+        if (IF_ID_IR[20:16] == 5'b00000) ID_EX_B <= 0;
+        else ID_EX_B <= #2 Reg[IF_ID_IR[20:16]];
 
-    if(if_id_ir[10:0]==11'b00000000000) id_ex_imm<=0;
-    else id_ex_imm<=#2 {{16{if_id_ir[15]}},{if_id_ir[15:0]}};
-    id_ex_ir<=if_id_ir;
-    id_ex_npc<=if_id_npc;
-
-    case (if_id_ir[31:26])
-        ADD: id_ex_optype<=rr_ALU;          // Add
-        SUB: id_ex_optype<=rr_ALU;          // Subtract
-        AND: id_ex_optype<=rr_ALU;          // And
-        OR: id_ex_optype<=rr_ALU;           // Or
-        ADDI: id_ex_optype<=ri_ALU;         // Add Immediate
-        SUBI: id_ex_optype<=ri_ALU;         // Subtract Immediate
-        ANDI: id_ex_optype<=ri_ALU;         // And Immediate
-        ORI:  id_ex_optype<=ri_ALU;         // Or Immediate
-        Load: id_ex_optype<=ri_Load;        // Load
-        Store:id_ex_optype<=ri_Store;       // Store
-        shiftR:id_ex_optype<=ri_shiftR;     // Shift Right
-        shiftL:id_ex_optype<=ri_shiftL;     // Shift Left
-        branch:id_ex_optype<=r_branch;      // Branch
-        Jmp:   id_ex_optype<=r_branch;      // Jump
-        Beq:   id_ex_optype<=r_branch;      // Branch if Equal
-        Blt:   id_ex_optype<=r_branch;      // Branch if Less Than
-        Bgt:   id_ex_optype<=r_branch;      // Branch if Greater Than
-        Nop:   id_ex_optype<=r_Nop;         // No Operation
-        Halt:  id_ex_optype<=r_Halt;        // Halt
-        default: id_ex_optype<=r_Nop;
-    endcase
-end
-//***********************************************************************\\
-// Execute
-always @(posedge clk1)
-begin
-    if(halted==0)
+        ID_EX_NPC <= #2 IF_ID_NPC;
+        ID_EX_IR <= #2 IF_ID_IR;
+        ID_EX_Imm <= #2 {{16{IF_ID_IR[15]}},{IF_ID_IR[15:0]}};
+        case (IF_ID_IR[31:26])
+          ADD, SUB, AND, OR, SLT, MUL: ID_EX_type <= #2 RR_ALU;
+          ADDI, SUBI, SLTI, SRTI, MOVI:    ID_EX_type <= #2 RM_ALU;
+          LW:                              ID_EX_type <= #2 LOAD;
+          SW:                              ID_EX_type <= #2 STORE;
+          BNEQZ, BEQZ :                    ID_EX_type <= #2 BRANCH;
+          HLT:                             ID_EX_type <= #2 HALT;
+          default:                         ID_EX_type <= #2 HALT;
+        endcase
+      end
+/*********************************************************************/
+  always @(posedge clk1)    // EX Stage
+    if (HALTED == 0)
+      begin
+        EX_MEM_type <= #2 ID_EX_type;
+        EX_MEM_IR <= #2 ID_EX_IR;
+        TAKEN_BRANCH <= #2 0;
+        case (ID_EX_type)
+          RR_ALU:   begin 
+            case (ID_EX_IR[31:26])
+              ADD :           EX_MEM_ALUOut <= #2 ID_EX_A + ID_EX_B;
+              SUB:            EX_MEM_ALUOut <= #2 ID_EX_A - ID_EX_B;
+              AND:            EX_MEM_ALUOut <= #2 ID_EX_A & ID_EX_B;
+              OR:             EX_MEM_ALUOut <= #2 ID_EX_A | ID_EX_B;
+              SLT:            EX_MEM_ALUOut <= #2 (ID_EX_A < ID_EX_B) ? 32'b1 : 32'b0;// trial
+              MUL:            EX_MEM_ALUOut <= #2 ID_EX_A * ID_EX_B;
+              default:        EX_MEM_ALUOut <= #2 32'hxxxxxxxx;
+            endcase
+          end
+          RM_ALU: begin
+            case (ID_EX_IR[31:26])
+              ADDI:           EX_MEM_ALUOut <= #2 ID_EX_A + ID_EX_Imm;
+              SUBI:           EX_MEM_ALUOut <= #2 ID_EX_A - ID_EX_Imm;
+              SLTI:           EX_MEM_ALUOut <= #2 ID_EX_A << ID_EX_Imm;
+              SRTI:           EX_MEM_ALUOut <= #2 ID_EX_A >> ID_EX_Imm;
+              MOVI:           EX_MEM_ALUOut <= #2 ID_EX_Imm;
+              default:        EX_MEM_ALUOut <= #2 32'hxxxxxxxx;
+            endcase
+          end
+          LOAD, STORE: begin
+                                EX_MEM_ALUOut <= #2 ID_EX_A + ID_EX_Imm;
+                                EX_MEM_B <= #2 ID_EX_B;
+          end
+          BRANCH: begin
+                                TAKEN_BRANCH <= #2 1'B1;
+                                EX_MEM_ALUOut <= #2 ID_EX_NPC + ID_EX_Imm;
+                                EX_MEM_cond <= #2 (ID_EX_A == 0);
+          end
+        endcase
+      end
+/*********************************************************************/
+  always @(posedge clk2)    // MEM Stage
     begin
-        ex_mem_ir<=id_ex_ir;
-        ex_mem_optype<=id_ex_optype;
-        taken_branch<=0;
-        case(id_ex_optype)
-            rr_ALU: case(id_ex_ir[31:26])
-                        ADD: ex_mem_ALUout <= id_ex_a + id_ex_b;    // ADD
-                        SUB: ex_mem_ALUout <= id_ex_a - id_ex_b;    // SUB
-                        AND: ex_mem_ALUout <= id_ex_a & id_ex_b;    // AND
-                        OR:  ex_mem_ALUout <= id_ex_a | id_ex_b;    // OR
-                        XOR: ex_mem_ALUout <= id_ex_a ^ id_ex_b;    // XOR
-                        default: ex_mem_ALUout <=0;
-                    endcase
-            ri_ALU: case(id_ex_ir[31:26])
-                        ADDI: ex_mem_ALUout <= id_ex_a + id_ex_imm; // ADDI
-                        SUBI: ex_mem_ALUout <= id_ex_a - id_ex_imm; // SUBI
-                        ANDI: ex_mem_ALUout <= id_ex_a & id_ex_imm; // ANDI
-                        ORI:  ex_mem_ALUout <= id_ex_a | id_ex_imm; // ORI
-                        default: ex_mem_ALUout <=0;
-                    endcase
-            ri_Load, ri_Store:
-            begin
-                ex_mem_ALUout <= id_ex_a + id_ex_imm;
-                ex_mem_b <= id_ex_b;
-            end
-            ri_shiftL,ri_shiftR:
-            begin
-                case(id_ex_ir[31:26])
-                    shiftL: ex_mem_ALUout <= id_ex_a << id_ex_imm; // Shift Left
-                    shiftR: ex_mem_ALUout <= id_ex_a >> id_ex_imm; // Shift Right
-                    default: ex_mem_ALUout <=0;
-                endcase
-            end
-            r_branch:
-            begin
-                case(id_ex_ir[31:26])
-                    branch:
-                    begin
-                        ex_mem_ALUout <= id_ex_npc + id_ex_imm;
-                        ex_mem_cond <= (id_ex_a==0)? 1:0;
-                    end
-                    Jmp:
-                    begin
-                        ex_mem_ALUout <= id_ex_ir[25:0];
-                        ex_mem_cond <= 1;
-                    end
-                    Beq:
-                    begin
-                        if(id_ex_a==id_ex_b)
-                        begin
-                            ex_mem_ALUout <= id_ex_npc + id_ex_imm;
-                            ex_mem_cond <= 1;
-                        end
-                        else begin
-                            ex_mem_cond <= 0;
-                        end
-                    end
-                    Blt:
-                    begin
-                        if(id_ex_a < id_ex_b)
-                        begin
-                            ex_mem_ALUout <= id_ex_npc + id_ex_imm;
-                            ex_mem_cond <= 1;
-                        end
-                        else begin
-                            ex_mem_cond <= 0;
-                        end
-                    end
-                    Bgt:
-                    begin
-                        if(id_ex_a > id_ex_b)
-                        begin
-                            ex_mem_ALUout <= id_ex_npc + id_ex_imm;
-                            ex_mem_cond <= 1;
-                        end
-                        else begin
-                            ex_mem_cond <= 0;
-                        end
-                    end
-                endcase
-            end
+    if (HALTED== 0)
+      begin
+        MEM_WB_type <= EX_MEM_type;
+        MEM_WB_IR <= #2 EX_MEM_IR;
+        case (EX_MEM_type)
+          RR_ALU, RM_ALU:       MEM_WB_ALUOut <= #2 EX_MEM_ALUOut; // Corrected spaces
+          LOAD:                 MEM_WB_LMD <= #2 Mem[EX_MEM_ALUOut];
+          STORE:
+            if (TAKEN_BRANCH == 1)
+              Mem[EX_MEM_ALUOut] <= #2 EX_MEM_B;
+        endcase
+      end
+    end
+/*********************************************************************/
+  always @ (posedge clk1)    // WB Stage
+    begin
+      if (TAKEN_BRANCH == 0)
+        case (MEM_WB_type)
+          RR_ALU:               Reg[MEM_WB_IR[15:11] ] <= #2 MEM_WB_ALUOut; 
+          RM_ALU:               Reg[MEM_WB_IR[20:16]] <= #2 MEM_WB_ALUOut;  
+          LOAD:                 Reg[MEM_WB_IR[20:16]] <= #2 MEM_WB_LMD; 
+          HALT:                 HALTED <= #2 1'b1;
+          default: ; 
         endcase
     end
-end
-//***********************************************************************\\
-// Memory
-always @(posedge clk2)
-begin
-    if(halted==0)
-    begin
-        mem_wb_optype <= ex_mem_optype;
-        mem_wb_ir <= ex_mem_ir;
-        case(ex_mem_optype)
-            rr_ALU, ri_ALU:
-                mem_wb_ALUout <= #2 ex_mem_ALUout;
-            ri_Load:
-                mem_wb_Loadout <= #2 mem[ex_mem_ALUout];
-            ri_Store:
-                if(taken_branch==0) mem[ex_mem_ALUout] <= #2 ex_mem_b;
-        endcase
-    end
-end
-//***********************************************************************\\
-// Write Back
-always @(posedge clk1)
-begin
-    if(taken_branch==0)
-    begin
-        case(mem_wb_optype)
-            rr_ALU:
-                regfile[mem_wb_ir[15:11]] <= #2 mem_wb_ALUout;
-            ri_ALU:
-                regfile[mem_wb_ir[20:16]] <=#2 mem_wb_ALUout;
-            ri_shiftL:
-                regfile[mem_wb_ir[20:16]] <=#2 mem_wb_ALUout;
-            ri_shiftR:
-                regfile[mem_wb_ir[20:16]] <=#2 mem_wb_ALUout;
-            ri_Load:
-                regfile[mem_wb_ir[20:16]] <=#2 mem_wb_Loadout;
-            r_Halt:
-                halted <= 1;
-        endcase
-    end
-end
-//***********************************************************************\\
+/*********************************************************************/
+
+
 endmodule
